8f655bbd22ebe76415f63eff73f4c695886becbd
Christof Weickhardt <christof.weickhardt@swisscom.com>
2021-03-24 23:30:27 +0100

    feat: make the mt-5 download parallel

--- download_all_fx_data.py
+++ download_all_fx_data.py
@@ -1,8 +1,8 @@
 import csv
 import os
+import multiprocessing
 
-from histdata.api import download_hist_data
-
+from histdata.api import download_hist_data, Platform
 
 def mkdir_p(path):
     import errno
@@ -13,40 +13,50 @@ def mkdir_p(path):
             pass
         else:
             raise
-
+def handle_download(row):
+    currency_pair_name, pair, history_first_trading_month = row
+    year = int(history_first_trading_month[0:4])
+    print('[START] for currency', currency_pair_name)
+    output_folder = os.path.join('output', pair)
+    mkdir_p(output_folder)
+    try:
+        while True:
+            could_download_full_year = False
+            try:
+                print('-', download_hist_data(year=year,
+                                                pair=pair,
+                                                output_directory=output_folder,
+                                                verbose=False))
+                could_download_full_year = True
+            except AssertionError:
+                pass  # lets download it month by month.
+            month = 1
+            while not could_download_full_year and month <= 12:
+                print('-', download_hist_data(year=str(year),
+                                                month=str(month),
+                                                pair=pair,
+                                                output_directory=output_folder,
+                                                platform=Platform.META_TRADER,
+                                                verbose=False))
+                month += 1
+            year += 1
+    except Exception:
+        print('[DONE] for currency', currency_pair_name)
 
 def download_all():
     with open('pairs.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',')
         next(reader, None)  # skip the headers
-        for row in reader:
-            currency_pair_name, pair, history_first_trading_month = row
-            year = int(history_first_trading_month[0:4])
-            print(currency_pair_name)
-            output_folder = os.path.join('output', pair)
-            mkdir_p(output_folder)
-            try:
-                while True:
-                    could_download_full_year = False
-                    try:
-                        print('-', download_hist_data(year=year,
-                                                      pair=pair,
-                                                      output_directory=output_folder,
-                                                      verbose=False))
-                        could_download_full_year = True
-                    except AssertionError:
-                        pass  # lets download it month by month.
-                    month = 1
-                    while not could_download_full_year and month <= 12:
-                        print('-', download_hist_data(year=str(year),
-                                                      month=str(month),
-                                                      pair=pair,
-                                                      output_directory=output_folder,
-                                                      verbose=False))
-                        month += 1
-                    year += 1
-            except Exception:
-                print('[DONE] for currency', currency_pair_name)
+
+        # Network should be the bottleneck
+        nr_parallel_jobs = os.cpu_count() * 2
+
+
+        pool = multiprocessing.Pool(nr_parallel_jobs)
+        rows = [row for row in reader]
+        
+        pool.map(handle_download, rows)
+            
 
 
 if __name__ == '__main__':
