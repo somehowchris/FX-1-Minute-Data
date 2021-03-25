b7c8d341edcc5798997927b9d951f970a5ddce6a
Christof Weickhardt <christof.weickhardt@swisscom.com>
2021-03-25 01:09:26 +0100

    feat: extract to main csv

--- download_all_fx_data.py
+++ download_all_fx_data.py
@@ -1,6 +1,8 @@
 import csv
 import os
 import multiprocessing
+from zipfile import ZipFile
+import pandas
 
 from histdata.api import download_hist_data, Platform
 
@@ -13,34 +15,79 @@ def mkdir_p(path):
             pass
         else:
             raise
+
+def extract_zip_to_folder(path):
+    from pathlib import Path
+    folder_name = ".".join(Path(path).name.split('.')[:-1])
+    parent_folder = Path(path).parent.absolute()
+
+    extract_folder = Path.joinpath(parent_folder, folder_name)
+    os.makedirs(extract_folder.absolute())
+
+    with ZipFile(path, 'r') as zip:
+        zip.extractall(path=extract_folder)
+        return extract_folder
+
 def handle_download(row):
     currency_pair_name, pair, history_first_trading_month = row
     year = int(history_first_trading_month[0:4])
     print('[START] for currency', currency_pair_name)
     output_folder = os.path.join('output', pair)
+
+    if os.path.exists(output_folder):
+        import shutil
+        shutil.rmtree(output_folder)
+
     mkdir_p(output_folder)
+
     try:
         while True:
             could_download_full_year = False
+            path = None
             try:
-                print('-', download_hist_data(year=year,
+                path = download_hist_data(year=year,
                                                 pair=pair,
                                                 output_directory=output_folder,
-                                                verbose=False))
+                                                platform=Platform.META_TRADER,
+                                                verbose=False)
+                print('-', path)
+                extract_zip_to_folder(path)
                 could_download_full_year = True
             except AssertionError:
                 pass  # lets download it month by month.
             month = 1
             while not could_download_full_year and month <= 12:
-                print('-', download_hist_data(year=str(year),
+                path = download_hist_data(year=str(year),
                                                 month=str(month),
                                                 pair=pair,
                                                 output_directory=output_folder,
                                                 platform=Platform.META_TRADER,
-                                                verbose=False))
+                                                verbose=False)
+                print('-', path)
+                extract_zip_to_folder(path)
                 month += 1
             year += 1
-    except Exception:
+            os.remove(path)
+    except Exception as e:
+        import glob
+        # combines all csvs
+        extension = 'csv'
+        all_filenames = [i for i in glob.glob('{}/**/*.{}'.format(output_folder, extension))]
+
+        file_name = "{}.csv".format(output_folder)
+        if os.path.exists(file_name):
+            os.remove(file_name)
+
+        combined_csv = open(file_name, 'a')
+
+        for file in all_filenames:
+            f = open(file)
+            for line in f:
+                combined_csv.write(line)
+
+        import shutil
+        shutil.rmtree(output_folder)
+
         print('[DONE] for currency', currency_pair_name)
 
 def download_all():
@@ -48,16 +95,20 @@ def download_all():
         reader = csv.reader(f, delimiter=',')
         next(reader, None)  # skip the headers
 
-        # Network should be the bottleneck
-        nr_parallel_jobs = os.cpu_count() * 2
+        nr_parallel_jobs = os.cpu_count()
 
 
         pool = multiprocessing.Pool(nr_parallel_jobs)
         rows = [row for row in reader]
-        
+
         pool.map(handle_download, rows)
             
 
 
 if __name__ == '__main__':
+    import time
+    start_time = time.time()
+
     download_all()
+
+    print("Took %s seconds to download, extract and join" % (time.time() - start_time))
